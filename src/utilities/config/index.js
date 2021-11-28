4f7451729ed9dbc250805ed6a60fe5de951d3442
lcameron3 <75503799+lcameron3@users.noreply.github.com>
2021-11-27 21:23:54 -0600

    Google Services updated keys

--- src/utilities/config/index.js
+++ src/utilities/config/index.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL = 'http://192.168.100.76:5000';
 export const SERVER_URL = 'https://5-dot-silo.appspot.com/';
 
-export const STRIPE_PUBLISHABLE_KEY = 'pk_live_R7mSgY4FR1nLIqV7a03EHECa';
-// export const STRIPE_PUBLISHABLE_KEY = 'pk_test_Hrdjvb968VdXnGc4HKYCiYSt';
+//export const STRIPE_PUBLISHABLE_KEY = 'pk_live_R7mSgY4FR1nLIqV7a03EHECa';
+export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51K0WTBAE76jHodFbhjAnRhfGbMviqqBbhQE4KNeOPkU4mWEqj2TqtBdKLbFJiulvSXvyoHvSaWG2JGp4HlMygkp100BsYX5n6D';
